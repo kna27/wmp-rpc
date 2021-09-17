564a6e8176df46ebd45138fe974a7fb0abce1576
Krish Arora <43799189+kna27@users.noreply.github.com>
2021-09-17 08:32:49 -0400

    Add idle detection

--- DiscordPresence.cs
+++ DiscordPresence.cs
@@ -21,19 +21,34 @@ namespace RemoteWindowsMediaPlayer
             {
                 WindowsMediaPlayerController wmpc = new WindowsMediaPlayerController();
                 SongDetails x = wmpc.GetCurrentSongDetails();
+                if (wmpc.GetPlayingState() == PlayingState.Playing || wmpc.GetPlayingState() == PlayingState.Paused)
+                {
+                    songName = wmpc.GetCurrentSongName();
+                    songPath = x.SongPath;
+                    artistName = x.SongName.Substring(0,
+                        x.SongName.LastIndexOf(" - " + wmpc.GetCurrentSongName().ToString()));
+                    albumName = "[Album name]";
 
-                songName = wmpc.GetCurrentSongName();
-                songPath = x.SongPath;
-                artistName = x.SongName.Substring(0,
-                    x.SongName.LastIndexOf(" - " + wmpc.GetCurrentSongName().ToString()));
-                albumName = "[Album name]";
-                durationTs = TimeSpan.FromSeconds(wmpc.GetCurrentSongTime());
-                duration = $"{durationTs.Minutes:D2}:{durationTs.Seconds:D2}";
-                lengthTs = TimeSpan.FromSeconds(x.Duration);
-                songLength = $"{lengthTs.Minutes:D2}:{lengthTs.Seconds:D2}";
+                    if (wmpc.GetPlayingState() == PlayingState.Paused)
+                    {
+                        duration = "Paused";
+                        songLength = "";
+                    }
+                    else
+                    {
+                        durationTs = TimeSpan.FromSeconds(wmpc.GetCurrentSongTime());
+                        duration = $"{durationTs.Minutes:D2}:{durationTs.Seconds:D2}";
+                        lengthTs = TimeSpan.FromSeconds(x.Duration);
+                        songLength = $"{lengthTs.Minutes:D2}:{lengthTs.Seconds:D2}";
+                    }
 
-                Console.WriteLine("Windows Media Player\n{0}\n{1} | {2}\n{3}  {4}\n\n",
-                songName, albumName, artistName, duration, songLength);
+                    Console.WriteLine("Windows Media Player\n{0}\n{1} | {2}\n{3}  {4}\n\n",
+                        songName, albumName, artistName, duration, songLength);
+                }
+                else
+                {
+                    Console.WriteLine("Windows Media Player\nIdle\n\n");
+                }
                 Thread.Sleep(1000);
             }
         }
