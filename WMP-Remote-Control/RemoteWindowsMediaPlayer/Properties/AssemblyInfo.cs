8a87595c45d782ed3d148b3ecfc8d0533c3a31d2
kna27 <krisharora27@gmail.com>
2021-09-19 13:02:24 -0400

    Update project metadata

--- WMP-Remote-Control/RemoteWindowsMediaPlayer/Properties/AssemblyInfo.cs
+++ WMP-Remote-Control/RemoteWindowsMediaPlayer/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RemoteWindowsMediaPlayer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WindowsMediaPlayerDiscordPresence")]
+[assembly: AssemblyDescription("Discord Rich Presence for Windows Media Player")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RemoteWindowsMediaPlayer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("kna27")]
+[assembly: AssemblyProduct("WindowsMediaPlayerDiscordPresence")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- WMP-Remote-Control/RemoteWindowsMediaPlayer/Properties/Resources.Designer.cs
+++ WMP-Remote-Control/RemoteWindowsMediaPlayer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RemoteWindowsMediaPlayer.Properties {
+namespace WindowsMediaPlayerDiscordPresence.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RemoteWindowsMediaPlayer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RemoteWindowsMediaPlayer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsMediaPlayerDiscordPresence.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
